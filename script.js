b894d0ed65c434720e93150a1c845be88eaaf79e
giul <giuliacos@virgilio.it>
2026-09-14 11:29:58 +0200

    Update script.js
    maxDistSatellite --> 0.5

--- script.js
+++ script.js
@@ -262,7 +262,7 @@ d3.select("body").append("div")
     // la simulazione con la barra spaziatrice.
     const cx = width / 2;
     const cy = height / 2;
-    const maxDistSatellite = Math.min(width, height) * 0.1;
+    const maxDistSatellite = Math.min(width, height) * 0.5;
 
     nodes.forEach(d => {
       if (d.fx != null || d.fy != null) return; // non toccare un nodo che si sta trascinando
