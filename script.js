38a7774afa1aefc0297aa5243e0cb65b2211f356
giul <giuliacos@virgilio.it>
2026-09-14 11:39:15 +0200

    Update script.js
    distance(90)) --> 130

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ d3.select("body").append("div")
     .range([15, 45]);
 
   simulation = d3.forceSimulation(nodes)
-    .force("link", d3.forceLink(links).id(d => d.scientific_name).distance(90))
+    .force("link", d3.forceLink(links).id(d => d.scientific_name).distance(130))
     .force("charge", d3.forceManyBody().strength(-350))
     .force("center", d3.forceCenter(width / 2, height / 2))
     .force("collide", d3.forceCollide(d => sizeScale(d.degree) + 5))
